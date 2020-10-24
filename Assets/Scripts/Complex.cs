5ce10020aa8ff1b44157740a771a551d5c66764e
Alex Uskov <FrozenAlex@users.noreply.github.com>
2020-10-24 16:04:11 +0300

    InitialCommit

---
+++ Assets/Scripts/Complex.cs
@@ -0,0 +1,55 @@
+namespace ComplexMath
+{
+	// Complex num
+	public class Complex
+	{
+		public double r;
+		public double i;
+
+		public Complex(double r, double i)
+		{
+			this.r = r;
+		}
+
+
+		// Complex operations 
+		public static Complex operator -(Complex a, Complex b)
+		{
+			return new Complex(a.r - b.r, a.i - b.i);
+		}
+
+		public static Complex operator -(Complex a, double b)
+		{
+			return new Complex(a.r - b, a.i);
+		}
+
+		public static Complex operator +(Complex a, Complex b)
+		{
+			return new Complex(a.r + b.r, a.i + b.i);
+		}
+
+		public static Complex operator +(Complex a, double b)
+		{
+			return new Complex(a.r + b, a.i);
+		}
+
+		public static Complex operator *(Complex a, Complex b)
+		{
+			double x, y;
+
+			x = a.r * b.r - a.i * b.i;
+			y = a.r * b.i + a.i * b.r;
+			return new Complex(x, y);
+		}
+
+		public static Complex operator *(Complex a, double b)
+		{
+			return new Complex(a.r*b, a.i*b);
+		}
+
+		public override string ToString()
+		{
+			return this.r.ToString() + "  " + this.i.ToString() + i;
+		}
+	}
+}
---
+++ Assets/Scripts/TextureDraw.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ComplexMath;
+
+public class TextureDraw : MonoBehaviour
+{
+	// Start is called before the first frame update
+	[SerializeField]
+	Texture2D texture;
+	[SerializeField]
+	Renderer m_ObjectRenderer;
+
+
+	public Color get_color(int x, int y)
+	{
+		return new Color(22, 22, 22, 12);
+	}
+
+	void Start()
+	{
+		//Fetch the GameObject's Renderer component
+		m_ObjectRenderer = GetComponent<Renderer>();
+
+		texture = new Texture2D(1024, 2048);
+		m_ObjectRenderer.materials[1].mainTexture = texture;
+
+		for (int y = 0; y < texture.height; y++)
+		{
+			for (int x = 0; x < texture.width; x++)
+			{
+				// Triangle thing	
+				// Color color = ((x & y) != 0 ? Color.white : Color.gray);
+				Color color = new Color(222,222,222,222);
+
+				texture.SetPixel(x, y, color);
+				Debug.Log(new Complex(1,0));
+			}
+		}
+		texture.Apply();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
