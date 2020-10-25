b6bebf0b79b024aa0eb56edb95a14986d8e858a8
Alex Uskov <frozenalex32@gmail.com>
2020-10-25 09:49:29 +0300

    Help

--- Assets/Scripts/Complex.cs
+++ Assets/Scripts/Complex.cs
@@ -1,55 +1,66 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace ComplexMath
 {
-	// Complex num
-	public class Complex
-	{
-		public double r;
-		public double i;
+    
+    // Complex num
+    public class Complex
+    {
+        public double r;
+        public double i;
 
-		public Complex(double r, double i)
-		{
-			this.r = r;
-		}
+        public Complex(double r, double i)
+        {
+            this.r = r;
+        }
 
 
-		// Complex operations 
-		public static Complex operator -(Complex a, Complex b)
-		{
-			return new Complex(a.r - b.r, a.i - b.i);
-		}
+        // Complex operations 
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.r - b.r, a.i - b.i);
+        }
 
-		public static Complex operator -(Complex a, double b)
-		{
-			return new Complex(a.r - b, a.i);
-		}
+        public static Complex operator -(Complex a, double b)
+        {
+            return new Complex(a.r - b, a.i);
+        }
 
-		public static Complex operator +(Complex a, Complex b)
-		{
-			return new Complex(a.r + b.r, a.i + b.i);
-		}
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.r + b.r, a.i + b.i);
+        }
 
-		public static Complex operator +(Complex a, double b)
-		{
-			return new Complex(a.r + b, a.i);
-		}
+        public static Complex operator +(Complex a, double b)
+        {
+            return new Complex(a.r + b, a.i);
+        }
 
-		public static Complex operator *(Complex a, Complex b)
-		{
-			double x, y;
+        public static Complex operator *(Complex a, Complex b)
+        {
+            double x, y;
 
-			x = a.r * b.r - a.i * b.i;
-			y = a.r * b.i + a.i * b.r;
-			return new Complex(x, y);
-		}
+            x = a.r * b.r - a.i * b.i;
+            y = a.r * b.i + a.i * b.r;
+            return new Complex(x, y);
+        }
 
-		public static Complex operator *(Complex a, double b)
-		{
-			return new Complex(a.r*b, a.i*b);
-		}
+        public static Complex operator *(Complex a, double b)
+        {
+            return new Complex(a.r * b, a.i * b);
+        }
 
-		public override string ToString()
-		{
-			return this.r.ToString() + "  " + this.i.ToString() + i;
-		}
-	}
+        public double Module()
+        {
+            return Mathf.Sqrt((float)(r * r + i * i));
+        }
+
+        public override string ToString()
+        {
+            return this.r.ToString() + "  " + this.i.ToString() + i;
+        }
+    }
 }
--- Assets/Scripts/TextureDraw.cs
+++ Assets/Scripts/TextureDraw.cs
@@ -1,48 +1,92 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ComplexMath;
 
 public class TextureDraw : MonoBehaviour
 {
-	// Start is called before the first frame update
-	[SerializeField]
-	Texture2D texture;
-	[SerializeField]
-	Renderer m_ObjectRenderer;
+    // Start is called before the first frame update
+    [SerializeField]
+    Texture2D texture;
+    [SerializeField]
+    Renderer m_ObjectRenderer;
+    [SerializeField]
+    int max_iterations = 30;
 
 
-	public Color get_color(int x, int y)
-	{
-		return new Color(22, 22, 22, 12);
-	}
 
-	void Start()
-	{
-		//Fetch the GameObject's Renderer component
-		m_ObjectRenderer = GetComponent<Renderer>();
 
-		texture = new Texture2D(1024, 2048);
-		m_ObjectRenderer.materials[1].mainTexture = texture;
+    public Color get_color(int x, int y)
+    {
+        var c = new Complex(0, 0);
+        var z = new Complex(0, 0);
 
-		for (int y = 0; y < texture.height; y++)
-		{
-			for (int x = 0; x < texture.width; x++)
-			{
-				// Triangle thing	
-				// Color color = ((x & y) != 0 ? Color.white : Color.gray);
-				Color color = new Color(222,222,222,222);
+        if (x % 5 == 0)
+        {
+            return new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            return new Color(0, 0, 0, 1);
+        }
 
-				texture.SetPixel(x, y, color);
-				Debug.Log(new Complex(1,0));
-			}
-		}
-		texture.Apply();
-	}
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
+    void Start()
+    {
+        //Fetch the GameObject's Renderer component
+        m_ObjectRenderer = GetComponent<Renderer>();
 
-	}
+        texture = new Texture2D(256, 512);
+
+        m_ObjectRenderer.materials[1].mainTexture = texture;
+
+        var left = -1.75;
+        var top = -0.25;
+        var xside = 0.25;
+        var yside = 0.45;
+
+        var height = texture.height;
+        var width = texture.width;
+
+        // Set scale
+        var scaleX = xside / width;
+        var scaleY = yside / height;
+
+        for (int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                var c = new Complex(x * scaleX + left, y * scaleY + top);
+                var z = new Complex(0, 0);
+
+                var count = 0;
+
+                while (z.Module() < 4 && count < max_iterations)
+                {
+                    z = (z * z) + c;
+                    count = count + 1;
+                }
+
+
+                var result = 1 - ((1 / max_iterations) * count);
+
+                // transform coordinates here
+                texture.SetPixel(x, y, new Color(result, result, result, 1));
+                // Triangle thing	
+                // Color color = ((x & y) != 0 ? Color.white : Color.gray);
+                //Color color = new Color(222, 222, 222, 222);
+
+
+            }
+        }
+        texture.Apply();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
